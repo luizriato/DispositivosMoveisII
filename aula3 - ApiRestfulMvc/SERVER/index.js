29be21ee64de06634bc1ef02e151056796703404
unknown <luiz.riato@outlook.com>
2025-09-03 20:57:26 -0300

    feat(aula3): envio de arquivos de aula

--- aula3 - ApiRestfulMvc/SERVER/index.js
+++ aula3 - ApiRestfulMvc/SERVER/index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+console.log('dotenv carregado:', process.env.MONGODB_URI);
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-const app = express();
-const port = 3000;
 
-// Middleware
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware   
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method'));
@@ -15,12 +19,22 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(methodOverride('X-Method-Override'));
 app.use(methodOverride('_method')); // Common usage with forms
 
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl)
+    .then(() => console.log('MongoDb connected...'))
+    .catch(err => console.log(err));
+
 // CORS headers (if needed beyond the `cors()` middleware)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 
+    res.header('Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept');
     next();
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`);
 });
 
 // Routes
@@ -28,7 +42,32 @@ app.get('/', (req, res) => {
     res.send({ status: 'ok' });
 });
 
-// Start server
-app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
+app.delete('/deletar', async (req, res) => {
+    let corpo = req.body.name;
+    await User.deleteOne({nome: corpo});
+    // res.send({ status: 'deletado' });
+    res.json({"status": "deletado com sucesso"});
+});
+
+app.delete('/deletar/:id', async (req, res) => {
+    let id = req.params.id;
+    await User.deleteOne({_id: req.params.id});
+    res.json({"status": "deletado com sucesso"});
+});
+
+var User = mongoose.model('Usuario', { nome: String });
+
+app.post('/inserir', (req, res) => {
+    let corpo = req.body.name;
+    let user = new User({ nome: corpo });
+    user.save().then(() => console.log('Usuário savo com sucesso'));
+    // res.send({ status: 'adicionado' })
+    res.json({"status": "adicionado com sucesso"});
+})
+
+app.put('/atualizar/:id', async (req, res) => {
+    let id = req.params.id;
+    let corpo = req.body.name;
+    await User.updateOne({_id: id}, {nome: corpo});
+    res.json({"status": "atualizado com sucesso"});
 });
